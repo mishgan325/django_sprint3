c38f737bde10b8f0957d507de74eceae609813e4
Michael <shibut.m.v@edu.mirea.ru>
2024-12-03 18:58:48 +0300

    Add Russian language to models and help_text

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -6,25 +6,69 @@ User = get_user_model()
 
 
 class Category(models.Model):
-    title = models.CharField('Имя', max_length=256)
+    title = models.CharField('Заголовок',
+                             max_length=256
+                             )
     description = models.TextField('Описание')
-    slug = models.SlugField('Slug')
-    is_published = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField('Идентификатор',
+                            help_text='Идентификатор страницы для URL; разрешены символы латиницы, цифры, дефис и подчёркивание.'
+                            )
+    is_published = models.BooleanField('Опубликовано',
+                                       default=True,
+                                       help_text='Снимите галочку, чтобы скрыть публикацию.'
+                                       )
+    created_at = models.DateTimeField('Добавлено',
+                                      auto_now_add=True
+                                      )
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'Категории'
 
 
 class Location(models.Model):
-    name = models.CharField('Название', max_length=256)
-    is_published = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField('Название места',
+                             max_length=256
+                             )
+    is_published = models.BooleanField('Опубликовано',
+                                       default=True
+                                       )
+    created_at = models.DateTimeField('Добавлено',
+                                      auto_now_add=True
+                                      )
+
+    class Meta:
+        verbose_name = 'местоположение'
+        verbose_name_plural = 'Местоположения'
 
 
 class Post(models.Model):
     title = models.CharField('Имя', max_length=256)
     text = models.TextField('Текст')
-    pub_date = models.DateField("Дата публикации", auto_now=False, auto_now_add=False)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    location = models.ForeignKey(Location, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    is_published = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    pub_date = models.DateField("Дата и время публикации",
+                                 auto_now=False,
+                                 auto_now_add=False,
+                                 help_text='Если установить дату и время в будущем — можно делать отложенные публикации.'
+                                 )
+    author = models.ForeignKey(User,
+                               verbose_name='Автор публикации',
+                               on_delete=models.CASCADE
+                               )
+    location = models.ForeignKey(Location,
+                                 verbose_name='Местоположение',
+                                 on_delete=models.CASCADE
+                                 )
+    category = models.ForeignKey(Category,
+                                 verbose_name='Категория',
+                                 on_delete=models.CASCADE
+                                 )
+    is_published = models.BooleanField('Опубликовано',
+                                       default=True
+                                       )
+    created_at = models.DateTimeField('Добавлено',
+                                      auto_now_add=True
+                                      )
+
+    class Meta:
+        verbose_name = 'публикация'
+        verbose_name_plural = 'Публикации'
